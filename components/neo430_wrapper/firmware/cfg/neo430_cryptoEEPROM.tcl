7a0b08e7765df0b16d84d237734e1ee06ce7317e
Adam Barcock <adam.barcock@stfc.ac.uk>
2021-09-29 10:38:36 +0100

    Updated Neo430 firmware to latest version.

--- components/neo430_wrapper/firmware/cfg/neo430_cryptoEEPROM.tcl
+++
@@ -1,5 +0,0 @@
-# Note: library setting supported in recent ipbb version.
-# This file should go away when updating the reference ipbb version.
-set_property library neo430 [get_files neo430_package.vhd]
-set_property library neo430 [get_files neo430_application_image_cryptoEEPROM.vhd]
-set_property library neo430 [get_files neo430_bootloader_image.vhd]
